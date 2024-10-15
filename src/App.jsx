e2393a118a79f1163f304899166d9b2b73dcba2a
Salome Karseladze <saly13071@gmail.com>
2024-10-15 13:32:39 +0400

    123

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
               <p className="locationPart">London</p>
             </div>
             <div className="line"></div>
+            <div>12345</div>
           </div>
         </div>
       </div>
